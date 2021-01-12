d70ba1dcbb9b3dd689e790870d17431f96d5b87d
gpalet <gpalet@xubio.com>
2021-01-12 20:59:26 -0300

    => creado CRUD categorias => agregado request de categorias => agregado creacion de categorias para models.sql

--- database/models.sql
+++ database/models.sql
@@ -14,11 +14,11 @@ create table IF NOT EXISTS productos
 create table IF NOT EXISTS categorias
 (
     id          serial                         not null
-        constraint productos_pkey
+        constraint categorias_pkey
             primary key,
     nombre      varchar(100)                   not null,
     descripcion varchar(500),
     activo      boolean          default true,
     created     timestamp        default now() not null,
-    update      timestamp,
+    update      timestamp
 );
--- src/github.com/paletgerardo/go-app-resto/app/handlers/handlerRequest.go
+++ src/github.com/paletgerardo/go-app-resto/app/handlers/handlerRequest.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/paletgerardo/go-app-resto/app/features/categorias"
 	"github.com/paletgerardo/go-app-resto/app/features/producto"
 	"github.com/paletgerardo/go-app-resto/core/config"
 	"github.com/rs/cors"
@@ -20,6 +21,12 @@ func HandlerRequest() {
 	router.HandleFunc("/productos/update", producto.Update).Methods("PUT")
 	router.HandleFunc("/productos/delete/{id}", producto.Delete).Methods("DELETE")
 	router.HandleFunc("/productos/get", producto.ReadAll).Methods("GET")
+
+	router.HandleFunc("/categorias/create", categorias.Create).Methods("POST")
+	router.HandleFunc("/categorias/get/{id}", categorias.Read).Methods("GET")
+	router.HandleFunc("/categorias/update", categorias.Update).Methods("PUT")
+	router.HandleFunc("/categorias/delete/{id}", categorias.Delete).Methods("DELETE")
+	router.HandleFunc("/categorias/get", categorias.ReadAll).Methods("GET")
 
 	// START APP
 	port := os.Getenv("PORT")
