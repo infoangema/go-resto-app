075fbe93ea7155ecc4f481af485714823e3f6225
gpalet <gpalet@xubio.com>
2021-02-15 20:40:33 -0300

    update url connection

--- core/db/connection.go
+++ core/db/connection.go
@@ -16,7 +16,7 @@ var PORT = os.Getenv("PORTDB")
 var NAME = os.Getenv("DBNAME")
 
 func GetConnectionToDB() *sql.DB {
-	URL := "postgres://gerrdev:9003@127.0.0.1:5432/restodb?sslmode=disable"
+	URL := "postgres://omttzarnqhjlda:01ead292afd329351220056870602ba07c46eceab148d3fd611ff02b755c187d@ec2-35-174-118-71.compute-1.amazonaws.com:5432/d2ch7fcu9q8og7"
 	//URL := "postgres://"+USER+":"+PASS+"@"+HOST+":"+PORT+"/"+ NAME +"?sslmode=disable"
 
 	connectionToDB, err := sql.Open("postgres", URL)
